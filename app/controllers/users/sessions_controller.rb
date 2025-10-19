2623a1a93806772190e91dfa901505dbd789749a
lakikishorsubba <lakikishor7@gmail.com>
2025-10-19 14:42:45 +0530

    add:devise jwt

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,10 @@
-# app/controllers/users/sessions_controller.rb
 class Users::SessionsController < Devise::SessionsController
-  # Keep HTML responses so your normal web flows remain intact
   respond_to :html, :json
 
-  # POST /users/sign_in
   def create
     super do |resource|
       token = request.env['warden-jwt_auth.token']
       if token.present?
-        # store encrypted cookie (HttpOnly)
         cookies.encrypted[:jwt] = {
           value: token,
           httponly: true,
